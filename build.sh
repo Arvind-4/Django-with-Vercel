9f6cab2dd9ccbf50299a36aa3c036f355fefca32
Arvind-4 <lordarvindthegamer@gmail.com>
2022-05-10 20:26:58 +0530

    Updated: build.sh with migrations

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
+
+echo "Installing dependencies..."
 python3.9 -m pip install -r requirements.txt
 
+echo "Migrating database..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Creating superuser..."
+python3.9 manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+
+echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
+
+
