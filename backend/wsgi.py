a16b29369a3386d3c82d722ce705e46b6bbc3f0e
Arvind-4 <lordarvindthegamer@gmail.com>
2022-05-10 21:39:09 +0530

    Added: Commands for migrations

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -16,3 +16,41 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 application = get_wsgi_application()
 
 app = application
+
+import subprocess
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DJANGO_SUPERUSER_USERNAME = str(os.environ.get("DJANGO_SUPERUSER_USERNAME"))
+DJANGO_SUPERUSER_PASSWORD = str(os.environ.get("DJANGO_SUPERUSER_PASSWORD"))
+DJANGO_SUPERUSER_EMAIL = str(os.environ.get("DJANGO_SUPERUSER_EMAIL"))
+
+migrate_command = [
+    "python3.9",
+    "manage.py",
+    "migrate",
+    "--noinput",
+]
+
+makemigration_command = [
+    "python3.9",
+    "manage.py",
+    "makemigrations",
+    "--noinput",
+]
+
+create_superuser_command = [
+    "python3.9",
+    "manage.py",
+    "createsuperuser",
+    "--email",
+    f"{DJANGO_SUPERUSER_EMAIL}",
+    "--username",
+    f"{DJANGO_SUPERUSER_USERNAME}",
+    "--noinput",
+]
+
+subprocess.run(makemigration_command)
+subprocess.run(migrate_command)
+subprocess.run(create_superuser_command)
