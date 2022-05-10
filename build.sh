cb57cde8665c6d1083753372f98b5fee30b1ff46
Arvind-4 <lordarvindthegamer@gmail.com>
2022-05-10 21:40:19 +0530

    Updated: buils.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,5 @@
 echo "Installing dependencies..."
 python3.9 -m pip install -r requirements.txt
 
-echo "Migrating database..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Creating superuser..."
-DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
-DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
-DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
-python manage.py createsuperuser --noinput || true
-
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
