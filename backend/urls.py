55e3d48933aeffbbb9e808f267e776cd3737fe20
Arvind-4 <lordarvindthegamer@gmail.com>
2022-07-08 20:12:46 +0530

    Added: new routes

--- backend/urls.py
+++ backend/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('about/', TemplateView.as_view(template_name='about.html')),
 ]
