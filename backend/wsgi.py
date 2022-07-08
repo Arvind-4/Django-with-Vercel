a2111b3d66bc3a594ae6e60443aae96c9a634c99
Arvind-4 <lordarvindthegamer@gmail.com>
2022-07-08 20:15:56 +0530

    Updated: to react environment variables

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,13 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-from gc import collect
-import os
+import os, pathlib, dotenv
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / '.env'
+
+if ENV_FILE.exists():
+	dotenv.read_dotenv(str(ENV_FILE))
+# dotenv.read_dotenv(str(ENV_FILE))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
